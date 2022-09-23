0a8efa21a60de61d89753751c72a177e8f6379bb
francis-xavier-inyangat <xavierfranzings@gmail.com>
2022-09-23 17:10:31 +0300

    code coverage added

--- test/produce.js
+++ test/produce.js
@@ -2,11 +2,6 @@ const assert = require('chai').assert;
 const price = require('../produce');
 describe('Finding total price of produce', ()=>{
 
-    // // a failing test
-    // it('Test 0', ()=>{
-    //     assert.equal(price(1000, 4500),4500000);
-    // })
-    
     it('Test 1', ()=>{
         assert.equal(price(1000, 4500),4500000);
     })
